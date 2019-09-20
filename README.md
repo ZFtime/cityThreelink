0c59b82fd78855b3000e27aaa0fead766818a2c3
ZFtime <15901453613@163.com>
2019-09-20 16:58:52 +0800

    选择某个城市，出现城市的三级联动

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# cityThreelink
-The Method of Three-Level Linkage in Cities
+# threelink
+
+> The Method of Three-Level Linkage in Cities
+
+## Getting started
+
+# clone the project
+
+git clone https://github.com/ZFtime/cityThreelink.git
+
+# enter the project directory
+
+cd cityThreelink
+
+# install dependencies
+
+npm install/cnpm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+## Build
+
+# build for production with minification
+
+npm run build
+
+# build for production and view the bundle analyzer report
+
+npm run build --report
+
---
+++ src/components/linkage.vue
@@ -0,0 +1,170 @@
+<template>
+  <div class="linkage">
+    <div>
+      <el-select v-model="sheng" @change="choseProvince" placeholder="请选择省份">
+        <el-option v-for="item in province" :key="item.id" :label="item.value" :value="item.id"></el-option>
+      </el-select>
+      <el-select v-model="shi" @change="choseCity" placeholder="请选择城市">
+        <el-option v-for="item in shi1" :key="item.id" :label="item.value" :value="item.id"></el-option>
+      </el-select>
+      <el-select v-model="qu" @change="choseBlock" placeholder="区级地区">
+        <el-option v-for="item in qu1" :key="item.id" :label="item.value" :value="item.id"></el-option>
+      </el-select>
+    </div>
+    <div>
+      <el-checkbox
+        :indeterminate="isIndeterminate"
+        v-model="checkAll"
+        @change="handleCheckAllChange"
+      >全选</el-checkbox>
+      <div style="margin: 15px 0;"></div>
+      <el-checkbox-group v-model="checkedCities" @change="handleCheckedCitiesChange">
+        <el-checkbox v-for="city in cities" :label="city" :key="city">{{city}}</el-checkbox>
+      </el-checkbox-group>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import axios from "axios";
+const cityOptions = ["北京市", "天津市", "上海市", "安徽省"];
+export default {
+  data() {
+    return {
+      mapJson: "../static/json/map.json",
+      province: "",
+      sheng: "",
+      shi: "",
+      shi1: [],
+      qu: "",
+      qu1: [],
+      city: "",
+      block: "",
+      // 复选框
+      checkAll: false,
+      checkedCities: ["北京市", "天津市", "安徽省"],
+      cities: cityOptions,
+      isIndeterminate: true,
+      checkedCity: []
+    };
+  },
+  methods: {
+    // 选中所有的标签
+    handleCheckAllChange(val) {
+      this.checkedCities = val ? cityOptions : [];
+      this.isIndeterminate = false;
+      this.getCityData();
+    },
+    // 选中某一个标签
+    handleCheckedCitiesChange(value) {
+      let checkedCount = value.length;
+      this.checkAll = checkedCount === this.cities.length;
+      this.isIndeterminate =
+        checkedCount > 0 && checkedCount < this.cities.length;
+      this.getCityData();
+    },
+    // 加载china地点数据，三级
+    getCityData: function() {
+      var that = this;
+      axios
+        .get(this.mapJson)
+        .then(function(response) {
+          if (response.status == 200) {
+            var data = response.data;
+            that.province = [];
+            that.city = [];
+            that.block = [];
+            // 省市区数据分类
+
+            for (var item in data) {
+              if (item.match(/0000$/)) {
+                //省
+                for (var cities in that.checkedCities) {
+                  if (that.checkedCities[cities] == data[item]) {
+                    that.province.push({
+                      id: item,
+                      value: data[item],
+                      children: []
+                    });
+                  }
+                }
+              } else if (item.match(/00$/)) {
+                //市
+                that.city.push({ id: item, value: data[item], children: [] });
+              } else {
+                //区
+                that.block.push({ id: item, value: data[item] });
+              }
+            }
+            // 分类市级
+            if (that.province) {
+              for (var index in that.province) {
+                for (var index1 in that.city) {
+                  if (
+                    that.province[index].id.slice(0, 2) ===
+                    that.city[index1].id.slice(0, 2)
+                  ) {
+                    that.province[index].children.push(that.city[index1]);
+                  }
+                }
+              }
+            }
+            // 分类区级
+            for (var item1 in that.city) {
+              for (var item2 in that.block) {
+                if (
+                  that.block[item2].id.slice(0, 4) ===
+                  that.city[item1].id.slice(0, 4)
+                ) {
+                  that.city[item1].children.push(that.block[item2]);
+                }
+              }
+            }
+          } else {
+            console.log(response.status);
+          }
+        })
+        .catch(function(error) {
+          console.log(typeof +error);
+        });
+    },
+    // 选省
+    choseProvince: function(e) {
+      if (this.province) {
+        for (var index2 in this.province) {
+          if (e === this.province[index2].id) {
+            this.shi1 = this.province[index2].children;
+            this.shi = this.province[index2].children[0].value;
+            this.qu1 = this.province[index2].children[0].children;
+            this.qu = this.province[index2].children[0].children[0].value;
+            this.E = this.qu1[0].id;
+          }
+        }
+      }
+    },
+    // 选市
+    choseCity: function(e) {
+      for (var index3 in this.city) {
+        if (e === this.city[index3].id) {
+          this.qu1 = this.city[index3].children;
+          this.qu = this.city[index3].children[0].value;
+          this.E = this.qu1[0].id;
+          // console.log(this.E)
+        }
+      }
+    },
+    // 选区
+    choseBlock: function(e) {
+      this.E = e;
+      // console.log(this.E)
+    }
+  },
+  created: function() {
+    this.getCityData();
+  }
+};
+</script>
+
+<style scoped>
+</style>
